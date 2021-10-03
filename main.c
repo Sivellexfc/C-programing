da31919c523af098f75ac2f0181bcc9b052b363c
Sivellexfc <91620498+Sivellexfc@users.noreply.github.com>
2021-10-03 16:48:01 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+
+	int kg;
+	printf("Kilogrami giriniz =");
+	scanf("%d",&kg);
+
+	if (kg<=0)
+	{
+	printf("Hata!");
+	}
+	else if(kg>100)
+	{
+	printf("Fazla aðýr");
+	}
+	else
+	{
+		if (kg%2==0)
+		{
+			printf("YES");
+		}
+
+		else
+		{
+			printf("NO");
+		}
+	}
+
+	return 0;
+}
+
