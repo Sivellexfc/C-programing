0789b3b1c077eea9297b0e625024718eb9a2dc31
Sivellexfc <91620498+Sivellexfc@users.noreply.github.com>
2021-10-06 17:13:33 +0300

    Add files via upload
    Taban alanı girilen dik eşkenar üçgeni yıldızlarla ekrana basar

---
+++ tabanAlaniXOlanUcgen.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main ()
+{
+     int tabanAlani;
+     int i=1;
+     int j=1;
+
+     printf("Taban Alaninin giriniz =");scanf("%d",&tabanAlani);
+    
+    for (i==1;i<=tabanAlani;i++)
+    {
+      
+        for (j==1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        j = 1;
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
